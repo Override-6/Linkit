cb934493385abfdb6d32ab446d91b05e72d14684
Maxime <maximebatista18@gmail.com>
2020-09-06 20:56:36 +0200

    de-extending AddressTaskCompleter from Task

--- RelayServer/src/fr/overridescala/vps/ftp/server/tasks/AddressTaskCompleter.scala
+++ RelayServer/src/fr/overridescala/vps/ftp/server/tasks/AddressTaskCompleter.scala
@@ -4,18 +4,15 @@ import java.net.InetSocketAddress
 
 import fr.overridescala.vps.ftp.api.packet.{PacketChannel, TaskPacket}
 import fr.overridescala.vps.ftp.api.task.{Task, TaskAchiever, TaskType, TasksHandler}
-import fr.overridescala.vps.ftp.api.utils.Protocol
 import fr.overridescala.vps.ftp.server.RelayServer
 
 class AddressTaskCompleter(private val packetChannel: PacketChannel,
                            private val server: RelayServer,
                            private val tasksHandler: TasksHandler,
                            private val id: String)
-        extends Task[InetSocketAddress] with TaskAchiever {
+        extends TaskAchiever {
 
     override val taskType: TaskType = TaskType.ADDRESS
-
-    override def enqueue(): Unit = tasksHandler.register(this, ownFreeWill = true)
 
     override def achieve(): Unit = {
         val address = server.getAddress(id)
@@ -23,12 +20,10 @@ class AddressTaskCompleter(private val packetChannel: PacketChannel,
             val errorMsg = "unable to retrieve the associated address from identifier"
             val errorResponse = new TaskPacket(taskType, "ERROR", errorMsg.getBytes)
             packetChannel.sendPacket(errorResponse)
-            error(errorMsg)
             return
         }
 
         val response = new TaskPacket(taskType, "OK", address.toString.getBytes)
         packetChannel.sendPacket(response)
-        success(address)
     }
 }
