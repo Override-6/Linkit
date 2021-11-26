22580ae3b5756a315503f7ef687ccff81a4baaf9
Override-6 <maximebatista18@gmail.com>
2021-11-26 18:28:57 +0100

    Updated NativeUtils.cpp

--- Natives/fr_linkit_engine_internal_utils_NativeUtils.cpp
+++ Natives/fr_linkit_engine_internal_utils_NativeUtils.cpp
@@ -1,12 +1,25 @@
 #include "fr_linkit_engine_internal_utils_NativeUtils.h"
 #include <jni.h>
+#include <iostream>
+
+using namespace std;
+
+jobject* GetJObjects(JNIEnv* env, jobjectArray array) {
+	const int len = env->GetArrayLength(array);
+	jobject* buff = new jobject[len];
+	for (int i = 0; i < len; i++) {
+		buff[i] = env->GetObjectArrayElement(array, i);
+	}
+	return buff;
+}
 
 JNIEXPORT void JNICALL Java_fr_linkit_engine_internal_utils_NativeUtils_callConstructor
   (JNIEnv* env, jclass clazz, jobject target, jstring signature, jobjectArray arguments) {
 	jclass targetClass = env->GetObjectClass(target);
 	const char* signatureUTF = env->GetStringUTFChars(signature, false);
 	jmethodID constructorID = env->GetMethodID(targetClass, "<init>", signatureUTF);
-	env->CallVoidMethod(target, constructorID, *arguments);
+	jobject* objects = GetJObjects(env, arguments);
+	env->CallVoidMethod(target, constructorID, **objects);
 }
 
 JNIEXPORT jobject JNICALL Java_fr_linkit_engine_internal_utils_NativeUtils_allocate
@@ -14,8 +27,3 @@ JNIEXPORT jobject JNICALL Java_fr_linkit_engine_internal_utils_NativeUtils_alloc
 	return env->AllocObject(target);
 }
 
-jobject* getJObjects(JNIEnv* env, jobjectArray array) {
-	const int len = env->GetArrayLength(array);
-	jobject test[len];
-
-}
