d631cab6d7b0941c85ad751a8fbe780b587cb462
Override-6 <maximebatista18@gmail.com>
2021-12-27 15:16:07 +0100

    starting writing behavior language parser

---
+++ Engine/src/main/scala/fr/linkit/engine/internal/language/bhv/parse/BhvFileReader.scala
@@ -0,0 +1,9 @@
+package fr.linkit.engine.internal.language.bhv.parse
+
+import java.util.Scanner
+
+class BhvFileReader(scanner: Scanner) {
+
+
+
+}
---
+++ Engine/src/test/scala/fr/linkit/engine/test/BehaviorLangParserTests.scala
@@ -0,0 +1,13 @@
+package fr.linkit.engine.test
+
+import org.junit.jupiter.api.Test
+
+class BehaviorLangParserTests {
+
+    @Test
+    def parse(): Unit = {
+        val behaviorSource = getClass.getResourceAsStream("/FSControl.bhv")
+
+    }
+
+}
