f959805accf0426374e8cb8c8a01dcb1ffbbf46b
Override-6 <maximebatista18@gmail.com>
2022-10-06 23:33:35 +0200

    Working on a better debugger

--- Engine/src/main/scala/fr/linkit/engine/internal/debug/Debugger.scala
+++ Engine/src/main/scala/fr/linkit/engine/internal/debug/Debugger.scala
@@ -2,11 +2,10 @@ package fr.linkit.engine.internal.debug
 
 import fr.linkit.api.application.connection.ConnectionContext
 import fr.linkit.api.internal.concurrency.Worker
-import fr.linkit.api.internal.system.log.AppLoggers
 import fr.linkit.engine.gnom.packet.traffic.AbstractPacketTraffic
-import org.apache.logging.log4j.Level
 
 import java.io.PrintStream
+import java.util.Scanner
 import java.util.concurrent.locks.LockSupport
 import scala.collection.mutable
 
@@ -30,7 +29,7 @@ object Debugger {
         //AppLoggers.Watchdog.debug("push - " + callerTrace + s"($act)")
         currentStack.push(act)
         act match {
-            case _: RequestAction => Watchdog.notifyNewRequestPending()
+            case _: RequestAction => DeadlockWatchdog.notifyNewRequestPending()
             case _                =>
         }
     }
@@ -75,6 +74,65 @@ object Debugger {
     }
 
 
+    private object DeadlockWatchdog extends Thread("Watchdog Thread") {
+        setDaemon(true)
 
+        start()
+
+        private var lastRequestPending: Long = 0
+
+
+        override def run(): Unit = {
+            while (true) {
+                LockSupport.park()
+                this.synchronized {
+                    val temp = lastRequestPending
+                    this.wait(5000) //waiting for 5 seconds
+
+                    def anyThreadIsWaiting: Boolean = {
+                        !threadStates.filterInPlace((t, st) => t.isAlive || !st.isEmpty)
+                                     .keys.exists(_.getState == Thread.State.RUNNABLE)
+                    }
+
+                    if (temp == lastRequestPending && anyThreadIsWaiting) {
+                        startDebuggerSession()
+                    }
+                }
+            }
+        }
+
+        def newTerminal(): Process = {
+            //TODO Support macos and windows
+            Runtime.getRuntime.exec("/usr/bin/x-terminal-emulator --disable-factory -e cat")
+        }
+
+        def startDebuggerSession(): Unit = {
+            val prompt = newTerminal()
+            val out    = new PrintStream(prompt.getOutputStream)
+            val in     = new Scanner(prompt.getInputStream)
+            makeDump(out)
+            var line = in.nextLine().trim
+            while (line != "exit") {
+                out.println(s"you entered: $line")
+                line = in.nextLine()
+            }
+        }
+
+        private def makeDump(out: PrintStream): Unit = {
+            out.println("It's looks like the application is facing a deadlock !")
+            out.println("------------------------------------------------------")
+
+            out.println("worker dump: ")
+            Debugger.dumpWorkers(out)
+            out.println("\nTraffic dump: ")
+            Debugger.dumpTraffic(out)
+        }
+
+        def notifyNewRequestPending(): Unit = {
+            lastRequestPending = System.currentTimeMillis()
+            LockSupport.unpark(this)
+        }
+
+    }
 
 }
